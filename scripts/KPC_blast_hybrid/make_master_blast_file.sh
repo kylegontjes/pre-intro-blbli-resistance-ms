b071dcee41ac432932883e1822d36ff09a0918b8
kylegontjes <kgontjes@umich.edu>
2025-06-25 11:37:27 -0400

    update figures with right number

---
+++ scripts/KPC_blast_hybrid/make_master_blast_file.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd /nfs/turbo/umms-esnitkin/Project_Penn_KPC/Analysis/Combination_resistance/pre-intro-blbli-resistance-ms/data/KPC_blast_hybrid
+# Curate results  
+first_isolate=$(ls *_clean.tsv | head -n1)
+## 0. Create file
+touch KPC_blast_master_hybrid.txt
+# 1. Add first line of first isolate
+cat $first_isolate | head -n1 >> KPC_blast_master_hybrid.txt
+for isolate in `ls *_clean.tsv`
+do 
+echo $isolate
+tail -n +2 $isolate  >> KPC_blast_master_hybrid.txt
+done
